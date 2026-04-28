2f3b64f0ac8ea2e81c15d5ac784cfbece77a1a0e
vinodhini-netti <nettivinodhini@gmail.com>
2026-04-28 15:54:18 +0530

    Add files via upload

---
+++ string(char).c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char str[10];
+    printf("Enter the string :\n");
+    scanf("%s",str);
+    for(int i=0;i<=strlen(str)-1;i++)
+    {
+        if(!(str[i]>='0'&&str[i]<='9'))
+        {
+            printf("%c\t",str[i]);
+        }
+        }
+}
